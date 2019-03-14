58b0a2f4e70ad249376a50930779787153c00733
Sam Guerrero <samguerrero@Sams-MacBook-Pro.local>
2019-03-13 21:38:16 -0700

    initial commit
    adding in angular js files to be hosted on express app

--- server.js
+++ server.js
@@ -1,54 +1,15 @@
-
 var express = require("express");
+var bodyParser = require('body-parser');
+var path = require("path");
+var app = express();
 var mongoose = require('mongoose');
 var moment = require('moment');
 
-mongoose.connect('mongodb://localhost/quotes', { useNewUrlParser: true });
+app.use(express.static( __dirname + '/public/dist/public' ));
+app.use(bodyParser.json());
 
-var QuoteSchema = new mongoose.Schema({
-    owner: {type: String, required: true, minlength: 3},
-    quote: {type: String, required: true, minlength: 3},
-    created_at: Date
-})
-mongoose.model('Quote', QuoteSchema)
-var Quote = mongoose.model('Quote')
-
-var path = require("path");
-
-var app = express();
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, "./static")));
-
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
-
-app.get('/', function(req, res) {
-    res.render("index");
-})
-
-app.post('/new_quote', function(req, res) {
-    console.log("POST DATA", req.body);
-    var quote = new Quote({owner: req.body.owner, quote: req.body.quote, created_at: new Date()});
-    quote.save(function(err) {
-        if(err) {
-            console.log(err)
-            console.log("something went wrong")
-            return res.redirect("/");
-        } else {
-            console.log("successfully added a user!")
-        }
-        res.redirect("/quotes");
-    })
-})
-
-app.get('/quotes', function(req, res) {
-    Quote.find({}).sort({created_at: -1}).exec(function(err, quotes) {
-        res.render("quotes", {quotes: quotes, moment:moment});
-    })
-})
+require('./server/config/mongoose.js');
+require('./server/config/routes.js')(app)
 
 app.listen(8000, function() {
     console.log("listening on port 8000");
